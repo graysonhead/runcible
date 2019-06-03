17ce712e66abe16198995a15d6cdbd5acfc644aa
graysonhead <grayson@graysonhead.net>
2019-06-03 09:56:52 -0500

    Bumped MergeDB Version to pull in Bugfix

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
         'colorama>=0.4.1',
         'cryptography==2.4.2',
         'pyyaml>=5.1',
-        'mergedb>=0.0.10'
+        'mergedb>=0.0.11'
     ],
     long_description=open('README.md').read(),
     entry_points={
