f209f258ee0ab680cc676c20f88492bffa06d0bd
graysonhead <grayson@graysonhead.net>
2019-07-16 23:51:02 -0500

    Fixed Version Dependency Deadlock with Paramiko
    This fixes issue #2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     install_requires=[
         'paramiko>=2.4.2',
         'colorama>=0.4.1',
-        'cryptography==2.4.2',
+        'cryptography>=2.5.0',
         'pyyaml>=5.1',
         'mergedb>=0.0.11'
     ],
