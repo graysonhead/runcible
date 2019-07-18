d773e28477edd8be813bb6c03c95ea68704e7a0f
graysonhead <grayson@graysonhead.net>
2019-07-17 22:25:07 -0500

    Added Support for Vlan Name Attribute
    #5

--- runcible/providers/cumulus/vlan.py
+++ runcible/providers/cumulus/vlan.py
@@ -5,16 +5,38 @@ from runcible.core.need import NeedOperation as Op
 
 class CumulusVlanProvider(SubProviderBase):
     provides_for = Vlan
-    supported_attributes = ['id']
+    supported_attributes = ['id', 'name']
+
+    @staticmethod
+    def get_cstate(id, vlan_commands):
+        configuration_dict = {}
+        configuration_dict.update({VlanResources.ID: id})
+        for line in vlan_commands:
+            if line[4] == 'alias':
+                configuration_dict.update({VlanResources.NAME: line[5]})
+        return Vlan(configuration_dict)
 
     def _create_vlan(self, vlan):
-        return self.device.send_command(f"net add bridge bridge vids {vlan}")
+        return self.device.send_command(f"net add vlan {vlan} vlan-id {vlan}")
 
     def _remove_vlan(self, vlan):
-        return self.device.send_command(f"net del bridge bridge vids {vlan}")
+        return self.device.send_command(f"net del vlan {vlan}")
+
+    def _set_vlan_name(self, vlan, name):
+        return self.device.send_command(f"net add vlan {vlan} alias {name}")
+
+    def _del_vlan_name(self, vlan):
+        return self.device.send_command(f"net del vlan {vlan} alias")
 
     def fix_need(self, need):
         if need.attribute == VlanResources.ID:
             if need.operation == Op.ADD:
                 pass
+        elif need.attribute == VlanResources.NAME:
+            if need.operation == Op.SET:
+                self._set_vlan_name(need.module, need.value)
+                self.complete(need)
+            elif need.operation == Op.DELETE:
+                self._del_vlan_name(need.module)
+                self.complete(need)
 
--- runcible/providers/cumulus/vlans.py
+++ runcible/providers/cumulus/vlans.py
@@ -1,6 +1,6 @@
 from runcible.providers.provider_array import ProviderArrayBase
 from runcible.modules.vlans import Vlans
-from runcible.modules.vlan import Vlan
+from runcible.modules.vlan import Vlan, VlanResources
 from runcible.providers.cumulus.vlan import CumulusVlanProvider
 from runcible.providers.cumulus.utils import extrapolate_list
 
@@ -16,15 +16,24 @@ class CumulusVlansProvider(ProviderArrayBase):
         return self.device.send_command(f"net del bridge bridge vids {vlan}")
 
     def get_cstate(self):
-        vlan_modules = []
-        commands = self.device.retrieve('parsed_commands')
-        for line in commands:
-            if line.startswith('net add bridge bridge vids'):
-                split_line = line.split(' ')
-                id_list = extrapolate_list(split_line[5].split(','), int_out=True)
-                for id in id_list:
-                    vlan_modules.append(Vlan({"id": id}))
         vlans_module = Vlans({})
-        vlans_module.vlans = vlan_modules
+        commands = self.device.retrieve('parsed_commands')
+        vlan_commands = {}
+        for line in commands:
+            if line.startswith('net add vlan'):
+                split_line = line.split(' ')
+                vlan_id = split_line[3]
+                if vlan_id not in vlan_commands:
+                    vlan_commands.update({vlan_id: []})
+                vlan_commands[vlan_id].append(split_line)
+        for key, value in vlan_commands.items():
+            vlans_module.vlans.append(CumulusVlanProvider.get_cstate(key, value))
+            # if line.startswith('net add bridge bridge vids'):
+            #     split_line = line.split(' ')
+            #     id_list = extrapolate_list(split_line[5].split(','), int_out=True)
+            #     for id in id_list:
+            #         vlan_modules.append(Vlan({"id": id}))
+        # vlans_module = Vlans({})
+        # vlans_module.vlans = vlan_modules
         return vlans_module
 
