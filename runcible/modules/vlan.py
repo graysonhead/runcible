f92e06c7def1bf1210aa81d36b1adbc20a402561
graysonhead <grayson@graysonhead.net>
2019-07-17 22:33:24 -0500

    Added Additional Attributes to Vlan Module

--- runcible/modules/vlan.py
+++ runcible/modules/vlan.py
@@ -5,6 +5,9 @@ from runcible.modules.module import Module
 class VlanResources(object):
     NAME = 'name'
     ID = 'id'
+    IPV4_ADDRESS = 'ipv4_address'
+    IPV4_GATEWAY = 'ipv4_gateway'
+    MTU = 'mtu'
 
 
 class Vlan(Module):
@@ -13,11 +16,23 @@ class Vlan(Module):
     configuration_attributes = {
         VlanResources.NAME: {
             'type': str,
-            'allowed_operations': [Op.DELETE, Op.ADD, Op.SET]
+            'allowed_operations': [Op.DELETE, Op.SET]
         },
         VlanResources.ID: {
             'type': int,
             'allowed_operations': [Op.DELETE, Op.ADD]
+        },
+        VlanResources.IPV4_ADDRESS: {
+            'type': str,
+            'allowed_operations': [Op.DELETE, Op.SET]
+        },
+        VlanResources.IPV4_GATEWAY: {
+            'type': str,
+            'allowed_operations': [Op.DELETE, Op.SET]
+        },
+        VlanResources.MTU: {
+            'type': int,
+            'allowed_operations': [Op.DELETE, Op.SET]
         }
     }
 
