f990370b59b9ea3eb301743c736e9cbfc3d6f875
graysonhead <grayson@graysonhead.net>
2019-08-21 21:25:11 -0500

    Moving Documentation back to Sphinx RTD Theme

--- docs/conf.py
+++ docs/conf.py
@@ -47,9 +47,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'rtcat_sphinx_theme'
-import rtcat_sphinx_theme
-html_theme_path = [rtcat_sphinx_theme.get_html_theme_path()]
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
