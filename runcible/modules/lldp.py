46599bf4363bcdefadd7448a5f7dbc1eaab1b86d
graysonhead <grayson@graysonhead.net>
2019-10-04 07:06:32 -0500

    LLDP Module Now Adds Adjacency Labels

--- runcible/modules/lldp.py
+++ runcible/modules/lldp.py
@@ -8,10 +8,10 @@ class LLDPResources(object):
 class LLDP(Module):
     module_name = 'lldp'
     configuration_attributes = {
-        LLDPResources.DISCOVER_ADJACENT: {
-            'type': bool,
-            'allowed_operations': [],
-            'examples': [True, False],
-            'descripion': ''
-        }
+        # LLDPResources.DISCOVER_ADJACENT: {
+        #     'type': bool,
+        #     'allowed_operations': [],
+        #     'examples': [True, False],
+        #     'descripion': ''
+        # }
     }
