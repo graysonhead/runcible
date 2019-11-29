b99324007fc2e1c8ec4422951f87a3905a7f72cc
graysonhead <grayson@graysonhead.net>
2019-11-28 22:51:26 -0600

    Removing Vyos Specific Modules

--- runcible/modules/static_v4_routes.py
+++
@@ -1,8 +0,0 @@
-from runcible.modules.module_array import ModuleArray
-from runcible.modules.static_v4_route import StaticV4Route, StaticV4RouteResources
-
-
-class StaticV4Routes(ModuleArray):
-    module_name = 'static_v4_routes'
-    sort_key = StaticV4RouteResources.PREFIX
-    sub_module = StaticV4Route
